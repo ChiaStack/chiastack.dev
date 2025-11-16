0e6dc9a8411d7f4289d26e4038f2c52c2acef0f0
Chia1104 <yuyuchia7423@gmail.com>
2025-11-17 01:07:33 +0800

    fix(docs): config nextjs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  reactCompiler: true,
+  transpilePackages: ["@chiastack/*", "@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  experimental: {
+    viewTransition: true,
+    authInterrupts: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default withMDX(config);
