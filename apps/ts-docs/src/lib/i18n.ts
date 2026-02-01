7f3875c09033a812ac6185d98748504daac511c0
Chia1104 <yuyuchia7423@gmail.com>
2026-02-02 04:39:21 +0800

    docs: remove tsdocs

--- apps/ts-docs/src/lib/i18n.ts
+++
@@ -1,8 +0,0 @@
-import { defineI18n } from "fumadocs-core/i18n";
-
-export const i18n = defineI18n({
-  parser: "dir",
-  languages: ["en"],
-  defaultLanguage: "en",
-  hideLocale: "default-locale",
-});
